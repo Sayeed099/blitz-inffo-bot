95991a7aa2cffb7fee32330428cc3c78cc72dee8
Sayidjalol <sayabdukhamidov@gmail.com>
2026-03-21 23:49:37 +0500

    .

--- api/bot.js
+++ api/bot.js
@@ -11,6 +11,9 @@ const LESSON1_VIDEO_FILE_ID =
 const BLITZ_CENTER_PHOTO_FILE_ID =
     'AgACAgIAAxkBAAIBpGm-23A6fAIRdyEtDO0C8qWylMxFAAKbFmsbXaT4SYO8PIz8vO9YAQADAgADeAADOgQ';
 
+/** Barcha filiallar uchun umumiy rasm file_id (keyin har filial uchun alohida almashtirasiz). Hozircha markaz rasmi. */
+const BRANCHES_PHOTO_FILE_ID = BLITZ_CENTER_PHOTO_FILE_ID;
+
 const USERS_JSON = path.join(__dirname, '..', 'users.json');
 
 function readRegisteredUserIds() {
@@ -61,6 +64,161 @@ function mainMenuKeyboard() {
         [BUTTONS.center],
         [BUTTONS.addresses]
     ]).resize();
+}
+
+function escapeHtml(text) {
+    return String(text).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
+const BRANCHES = [
+    {
+        titleLine: "Blitz nemis tili markazi | Mirzo Ulug'bek filiali",
+        address: "Shoh, Bunyodkor shoh ko'chasi, 100003, Toshkent, Toshkent, Uzbekistan",
+        directionsPrefix: "Metro",
+        directionsText: "Mirzo Ulug'bek",
+        bus: "150 | 2 | 94 | 98 | 33 | 114 | 150 | 9T | 56 | 23 | 146",
+        phone: "+9987811377161",
+        yandexUrl:
+            "https://yandex.uz/maps/org/155558165926/?ll=69.213441%2C41.283964&z=16.63",
+        googleUrl:
+            "https://www.google.com/maps/place/Blitz+nemis+tili+markazi/@41.2839105,69.2136383,213m/data=!3m1!1e3!4m6!3m5!1s0x38ae8b411b649a3b:0xb94da3066cbfead7!8m2!3d41.2839583!4d69.2133769!16s%2Fg%2F11ssrrr592?entry=ttu&g_ep=EgoyMDI1MTEyMy4xIKXMDSoASAFQAw%3D%3D",
+        telegramUrl: "https://t.me/Blitz_admin1",
+    },
+    {
+        titleLine: "Blitz nemis tili markazi | Integro filiali",
+        address: "Bunyodkor Avenue 52, 765625, Tashkent, Uzbekistan",
+        directionsPrefix: "Metro",
+        directionsText: "Chilonzor",
+        bus: "114 | 116 | 41 | 70 | 131 | 135 | 69 | 99 | 41 | 139 | 48",
+        phone: "+9987811377161",
+        yandexUrl:
+            "https://yandex.uz/maps/org/183021158302/?ll=69.205131%2C41.277337&z=16.63",
+        googleUrl:
+            "https://www.google.com/maps/@41.2771047,69.2059998,39m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI2MDMxOC4xIKXMDSoASAFQAw%3D%3D",
+        telegramUrl: "https://t.me/blitz_integro",
+    },
+    {
+        titleLine: "Blitz nemis tili markazi | Alisher Navoiy filiali",
+        address: "Navoi Avenue 27, 100030, Tashkent, Uzbekistan",
+        directionsPrefix: "Metro",
+        directionsText: "Alisher navoiy",
+        bus: "100 | 115 | 152 | 17 | 28 | 29 | 64 | 65 | 89 | 91 | 35 | 43 | 46 | 68 | 123",
+        phone: "+9987811377161",
+        yandexUrl:
+            "https://yandex.uz/maps/org/60328673317/?ll=69.250512%2C41.320802&z=16.67",
+        googleUrl:
+            "https://www.google.com/maps/place/GSR+Logistics/@41.3209419,69.2526813,39m/data=!3m1!1e3!4m6!3m5!1s0x2d89e769398f9903:0xc9bbc362df8d867f!8m2!3d41.3209165!4d69.2527776!16s%2Fg%2F11xzs9sdtx?entry=ttu&g_ep=EgoyMDI2MDMxOC4xIKXMDSoASAFQAw%3D%3D",
+        telegramUrl: "https://t.me/blitz_navoiy2",
+    },
+    {
+        titleLine: "Blitz nemis tili markazi | Bodomzor filiali",
+        address: "Amir Temur Avenue 100-A, 100021, Tashkent, Uzbekistan",
+        directionsPrefix: "Metro",
+        directionsText: "Bodomzor",
+        bus: "115 | 140 | 19 | 24 | 67 | 91 | 93 | 95 | 51",
+        phone: "+9987811377161",
+        yandexUrl:
+            "https://yandex.uz/maps/10335/tashkent/house/YkAYdA9lQUECQFprfX9yeX9gZw==/?ll=69.285891%2C41.338149&mode=search&sctx=ZAAAAAgBEAAaKAoSCR%2BF61G4VFFAEYekFkompURAEhIJDQBV3LjF4j8Rn5RJDW0A0T8iBgABAgMEBSgKOABAo58GSAFqAnV6nQHNzMw9oAEAqAEAvQEsHQp6ggIFQmxpdHqKAgCSAgCaAgxkZXNrdG9wLW1hcHM%3D&sll=69.285891%2C41.338149&sspn=0.001154%2C0.000522&text=Blitz&z=20.24",
+        googleUrl:
+            "https://www.google.com/maps/place/Makro/@41.3382182,69.2855019,78m/data=!3m1!1e3!4m9!1m2!2m1!1sBodomzor+metro!3m5!1s0x38ae8bc7af1eec55:0xf2aa8a864c42b3e0!8m2!3d41.3382119!4d69.2857387!16s%2Fg%2F11vl4ks0z4?entry=ttu&g_ep=EgoyMDI2MDMxOC4xIKXMDSoASAFQAw%3D%3D",
+        telegramUrl: "https://t.me/Blitz_Bodomzor",
+    },
+    {
+        titleLine: "Blitz nemis tili markazi | Samarqand filiali",
+        address: "Mirzo Ulugbek St 11, Samarkand, Samarqand Region, Uzbekistan",
+        directionsPrefix: "Mo'ljal",
+        directionsText:
+            "Mirzo Ulug'bek ko'chasi, Makon Mall savdo markazining ro'parasida",
+        bus: "6 | 3 | T3 | 87 | 77 | M1 | 22 | 52 | 87 | 66 | 77 | 64",
+        phone: "+9987811377161",
+        yandexUrl:
+            "https://yandex.uz/maps/org/165906105443/?indoorLevel=1&ll=66.958908%2C39.655430&mode=search&sctx=ZAAAAAgBEAAaKAoSCddOlIREvVBAEe8a9KW300NAEhIJNBE2PL1SZj8Rtr5IaMu5VD8iBgABAgMEBSgKOABA3lBIAWoCdXqdAc3MzD2gAQCoAQC9ASwdCnrCAQbjiJmG6gSCAgVibGl0eooCAJICAJoCDGRlc2t0b3AtbWFwcw%3D%3D&sll=66.958908%2C39.655430&sspn=0.005085%2C0.002360&text=blitz&z=18.1",
+        googleUrl:
+            "https://www.google.com/maps/@39.6555682,66.9589968,40m/data=!3m1!1e3!5m1!1e2?entry=ttu&g_ep=EgoyMDI2MDMxOC4xIKXMDSoASAFQAw%3D%3D",
+        telegramUrl: "https://t.me/Blitz_Samarqand",
+    },
+    {
+        titleLine: "Blitz nemis tili markazi | Narpay filiali",
+        address: "WWQJ+5R8, Galyakasab, Samarqand Region, Uzbekistan",
+        directionsPrefix: "Mo'ljal",
+        directionsText: "Narpay tuman 2-Kasb hunar maktabi",
+        bus: null,
+        phone: "+9987811377161",
+        yandexUrl:
+            "https://yandex.uz/maps/org/185342332981/?ll=65.923495%2C39.962811&z=16.88",
+        googleUrl:
+            "https://www.google.com/maps/search/+Samarqand+viloyati,+Narpay+tumani,+Mirbozor+shaharchasi/@39.9619432,65.9197767,318m/data=!3m1!1e3!5m1!1e2?entry=ttu&g_ep=EgoyMDI2MDMxOC4xIKXMDSoASAFQAw%3D%3D",
+        telegramUrl: "https://t.me/Blitz_Narpay",
+    },
+    {
+        titleLine: "Blitz nemis tili markazi | Farg'ona filiali",
+        address: "Komus Street 51, Fergana, Fergana Region, Uzbekistan",
+        directionsPrefix: "Mo'ljal",
+        directionsText: "Yuksalish ko'chasi, Beeline ofisining ro'parasida",
+        bus: "1 | 4 | 6",
+        phone: "+9987811377161",
+        yandexUrl: "https://yandex.uz/maps/-/CHT5FNMx",
+        googleUrl:
+            "https://www.google.com/maps/place/MeHnuD/@40.3858724,71.7909068,39m/data=!3m1!1e3!4m9!1m2!2m1!1z0KTQtdGA0LPQsNC90LAgbWFoaW16YWRlIGthZmU!3m5!1s0x38bb830037afa8e9:0xe4a9210a25dd10df!8m2!3d40.3858671!4d71.7910764!16s%2Fg%2F11vwdxz6gl?entry=ttu&g_ep=EgoyMDI2MDMxOC4xIKXMDSoASAFQAw%3D%3D",
+        telegramUrl: "https://t.me/blitz_fargona",
+    },
+    {
+        titleLine: "Blitz nemis tili markazi | Andijon filiali",
+        address: "149A, Milliy Tiklanish koʻchasi, Andijon Region, Uzbekistan",
+        directionsPrefix: "Mo'ljal",
+        directionsText:
+            "Amir Temur Shoh ko'chasi, Bobour haykali yon tarafida",
+        bus: null,
+        phone: "+9987811377161",
+        yandexUrl:
+            "https://yandex.uz/maps/10329/andijan/?ll=72.352922%2C40.762585&mode=poi&poi%5Bpoint%5D=72.352885%2C40.762563&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D107270238555&z=21",
+        googleUrl:
+            "https://www.google.com/maps/@40.7625097,72.3527565,45m/data=!3m1!1e3!5m1!1e2?entry=ttu&g_ep=EgoyMDI2MDMxOC4xIKXMDSoASAFQAw%3D%3D",
+        telegramUrl: "https://t.me/Blitz_Andijon",
+    },
+];
+
+function formatBranchCaption(b) {
+    const lines = [
+        `📍 <b>${escapeHtml(b.titleLine)}</b>`,
+        `🏠 ${escapeHtml(b.address)}`,
+        `🚇 ${escapeHtml(b.directionsPrefix)}: ${escapeHtml(b.directionsText)}`,
+    ];
+    if (b.bus) lines.push(`🚌 Bus: ${escapeHtml(b.bus)}`);
+    lines.push(`☎️ ${escapeHtml(b.phone)}`);
+    return lines.join("\n");
+}
+
+function branchesPickerKeyboard() {
+    const labels = [
+        "Mirzo Ulug'bek",
+        "Integro",
+        "Alisher Navoiy",
+        "Bodomzor",
+        "Samarqand",
+        "Narpay",
+        "Farg'ona",
+        "Andijon",
+    ];
+    const rows = [];
+    for (let i = 0; i < labels.length; i += 2) {
+        const row = [
+            Markup.button.callback(labels[i], `br:${i}`),
+            Markup.button.callback(labels[i + 1], `br:${i + 1}`),
+        ];
+        rows.push(row);
+    }
+    return Markup.inlineKeyboard(rows);
+}
+
+function branchLinksKeyboard(b) {
+    return Markup.inlineKeyboard([
+        [
+            Markup.button.url("🌍 Yandex Map", b.yandexUrl),
+            Markup.button.url("🌍 Google Map", b.googleUrl),
+            Markup.button.url("📨 Telegram", b.telegramUrl),
+        ],
+    ]);
 }
 
 // --- START ---
@@ -160,7 +318,34 @@ bot.hears(BUTTONS.center, async (ctx) => {
         return ctx.reply(caption, { parse_mode: 'HTML' });
     }
 });
-bot.hears(BUTTONS.addresses, (ctx) => ctx.reply("📍 <b>Manzilimiz:</b> Toshkent shahri, Blitz markazi.\n📞 Aloqa: +998...", { parse_mode: 'HTML' }));
+bot.hears(BUTTONS.addresses, (ctx) => {
+    return ctx.reply("Kerakli filialni tanlang:", branchesPickerKeyboard());
+});
+
+bot.action(/^br:(\d+)$/, async (ctx) => {
+    const idx = Number(ctx.match[1]);
+    const b = BRANCHES[idx];
+    if (!b) {
+        return ctx.answerCbQuery("Filial topilmadi");
+    }
+    await ctx.answerCbQuery();
+    const caption = formatBranchCaption(b);
+    const links = branchLinksKeyboard(b);
+    const photoId = BRANCHES_PHOTO_FILE_ID || BLITZ_CENTER_PHOTO_FILE_ID;
+    try {
+        if (photoId) {
+            return await ctx.replyWithPhoto(photoId, {
+                caption,
+                parse_mode: "HTML",
+                ...links,
+            });
+        }
+        return await ctx.reply(caption, { parse_mode: "HTML", ...links });
+    } catch (e) {
+        console.error("Filial rasm/xabar yuborishda xato", e);
+        return ctx.reply(caption, { parse_mode: "HTML", ...links });
+    }
+});
 
 // --- VERCEL EXPORT ---
 module.exports = async (req, res) => {
